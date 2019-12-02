82b2318248f7a596099c158fe9b0e800f1d9fe80
Jeriel Juarbe <jeriel.juarbe@gmail.com>
2019-12-01 21:23:50 -0500

    Update UbuntuSrv-SoftwareSetup.sh

--- UbuntuSrv-SoftwareSetup.sh
+++ UbuntuSrv-SoftwareSetup.sh
@@ -2,23 +2,23 @@
 # Ubuntu 18.04 LTS Server
 # RUN AS ROOT or with SUDO priviledges
 
-# Variables for the BASH script
+# Variables for the this script
 CALDERA_BUILD_DIR='/opt/caldera'
 CALDERA_VOLUME='/opt/caldera/caldera-persistent-volume'
 CALDERA_SSH='/opt/caldera/ssh'
 
-# Install Ansible and Docker with Dependencies on the host machine
+# Install Ansible
 apt update -qq && apt upgrade -qq
 apt install -y software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt install -y ansible
 
-# Install Docker and Dependencies
+# Install Docker and GIT
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common python-pip
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -qq
-apt-get install -y docker-ce docker-ce-cli containerd.io
+apt-get install -y docker-ce docker-ce-cli containerd.io git
 usermod -aG docker $USER
 systemctl enable docker
 
@@ -26,7 +26,7 @@ systemctl enable docker
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Create folder to host docker container build and persistent volume
+# Create folder for the docker container files and persistent volume
 mkdir -p $CALDERA_BUILD_DIR
 mkdir -p $CALDERA_VOLUME
 
@@ -40,9 +40,3 @@ chown -R $USER:$USER $CALDERA_BUILD_DIR
 cd $CALDERA_BUILD_DIR
 git clone https://github.com/jeriel20/ansible-docker-caldera.git
 cd ansible-docker-caldera
-
-# ADD docker build or docker compose up
-
-# run ansible playbook that configures the container caldera
-
-# ADD open chrome browser to navigate to weg UI
